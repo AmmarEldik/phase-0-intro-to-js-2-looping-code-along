6ead3a72c048788ea4ccc35927edb783fd4efae9
AmmarEldik <vektorea60@gmail.com>
2022-10-19 20:57:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const gifts = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(gifts) {
+    let newArray = [];
+     for (let i = 0; i < gifts.length; i++) { 
+ newArray.push(`Thank you, ${gifts[i]}, for the wonderful surprise gift!`);
+debugger;
+}
+return newArray
+}
+
+writeCards(gifts);
+let num = 0;
+function countDown(num) {
+    let countup = 0;
+while (countup < 11) {
+  console.log(countup++);
+}
+}
+console.log("num")
